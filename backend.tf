87f4bde55d0d5efcac05ee327e9a00b7025798d7
Divisha2408 <divisha.aahuja@gmail.com>
2026-04-22 01:17:06 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "demo-resources"
-    storage_account_name = "demostorageaccount2408" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "demostorageaccount24089" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "prodtfstate"            # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.                              # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.   
   }
